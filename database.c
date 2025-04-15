2cf1a09eea6f3b7af8991510725ad421dee549a7
Gajanraj MOHANARAJ <gajanraj.m@gmail.com>
2025-04-15 12:24:00 +0200

    ajout de la recherche en "dur"

--- database.c
+++ database.c
@@ -11,14 +11,15 @@
     int age; 
     char role[15]; 
   
-  }Users;
+  }Users; // Struct pour les colonnes des users
 
   typedef struct node {
     Users row; 
     struct node *left; 
     struct node *right; 
 
-  }node;
+  }node; // Struc pour les noeuds
+
 //Notions a revoir : Récursivié ! 
 
   node *insert_node(node* root, Users users1){
@@ -53,6 +54,20 @@ void save_data_csv(node* root, FILE* file) {
   save_data_csv(root->right, file);
 }
 
+node* searchUser(node *root, int id){
+  if (root == NULL){
+    printf("rien n'a été trouveé ");
+    return NULL; 
+  }
+  if(id == root->row.id){
+    return root;  // des datas on été trouvé donc on les return
+  }else if (id<root->row.id){
+    return searchUser(root->left, id); //on va chercher a gauche
+  }else{
+    return searchUser(root->right, id); // on va chercher a droite
+  }
+}
+
   int main(){
   node *root = NULL; // c'est pour initialisern, la racine de l'arbre a nulll (vick, pas oublier la struct)
   Users users1 = {1, "vick", 21,"member"} ; 
@@ -61,17 +76,31 @@ void save_data_csv(node* root, FILE* file) {
    root = insert_node(root, users1);
    root = insert_node(root, users2);
    root = insert_node(root, users3);
-    print_tree(root);
+  print_tree(root);
 
+    //***************** */ pour faire de la persistance********************* */
     FILE *file = fopen("usersTable.csv", "w");
     if (file == NULL) {
         printf("Erreur lors de la création du fichier.\n");
         return 1;
     }
-
     // premiere ligne du fichier
     fprintf(file, "ID,Name,Age,Role\n");
         save_data_csv(root, file);
     fclose(file); //Pas oblier de fermer le fichier
+
+    //*********************************fin de la persistance *********************************/
+
+    int findById = 2; 
+    node *userFound = searchUser(root, findById); 
+
+    if(userFound != NULL){
+      printf("user trouvé!");
+      printf("\nUtilisateur trouvé : ID: %d, Nom: %s, Age: %d, Role: %s\n", userFound->row.id, userFound->row.name, userFound->row.age, userFound->row.role); 
+    }else{
+      printf("User non trouvé");
+
+    }
+
     return 0;
   }
