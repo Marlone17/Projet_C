a6686171fccc7ba40d67c4375553f310a2c7633c
marlone17 <mbertuit@myges.fr>
2025-04-21 12:53:49 +0200

    Add: socket bind

--- server.c
+++ server.c
@@ -7,24 +7,33 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define PORT 8080 
+#define PORT 8080
+#define SA struct sockaddr
 
 int main(){
-    int sockfd, connfd, len; 
+    int socketo, conn, len; 
     struct sockaddr_in servaddr, cli;
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = htons(PORT); 
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
+    socketo = socket(AF_INET, SOCK_STREAM, 0); 
+    if (socketo == -1) { 
         printf("socket creation failed\n");
-        printf("sockfd == %s", sockfd); 
+        printf("socketo == %s", socketo); 
         exit(0); 
     } 
     else
         printf("Socket created\n");
-        printf("sockfd == %s\n", sockfd);
+        printf("socketo == %s\n", socketo);
     bzero(&servaddr, sizeof(servaddr)); 
 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(PORT); 
+    if ((bind(socketo, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+        printf("socket bind failed\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket bind\n"); 
+
+    
 }
